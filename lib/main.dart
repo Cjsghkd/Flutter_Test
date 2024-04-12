fea90d017d38371acc6235e13994147891dce108
SungHyun <pjhomg5530@jointree.co.kr>
2024-04-12 17:23:30 +0900

    :tada: :: 클린 아키텍처 + MVVM 적용

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:sample_project/presentation/view/SearchPage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const SearchPage(),
+    );
+  }
+}
